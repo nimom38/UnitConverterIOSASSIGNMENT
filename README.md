a4f0fa33f226456df9e8a0f2e60b8ac37f4df436
Gazi Momin Iqbal <gazimiqbal@gmail.com>
2024-05-05 22:20:53 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+<img width="158" alt="image" src="https://github.com/MohitSinghvi/UnitConverterIOS/assets/35193178/ebd925d2-c461-492f-b722-ea855597b8d1">
---
+++ UnitConverter/UnitConverterViewController.h
@@ -0,0 +1,20 @@
+//
+//  UnitConverterViewController.h
+//  UnitConverter
+//
+//  Created by Neil Smyth on 9/17/13.
+//  Copyright (c) 2013 Neil Smyth. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UnitConverterViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UITextField *tempText;
+@property (weak, nonatomic) IBOutlet UILabel *resultLabel;
+@property (nonatomic, weak) IBOutlet UITextField *milesText;
+@property (nonatomic, weak) IBOutlet UILabel *distanceResultLabel;
+- (IBAction)convertTemp:(id)sender;
+- (IBAction)convertDistance:(id)sender;
+
+
+@end
